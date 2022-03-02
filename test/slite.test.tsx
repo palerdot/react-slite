e2dd31bab0c4c951958218ca358f28ed8cf5ee72
palerdot <palerdot@gmail.com>
2022-03-02 12:39:25 +0530

    test: fix tests again

--- test/slite.test.tsx
+++ test/slite.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Default as Slite } from '../stories/Slite.stories'
+import Slite from '../src/'
+
+function DefaultSlite() {
+  return (
+    <Slite initialValue={[]} onChange={() => {}}>
+      {null}
+    </Slite>
+  )
+}
 
 describe('Slite', () => {
   test('render without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Slite initialValue={[]} onChange={() => {}} />, div)
+    ReactDOM.render(<DefaultSlite />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
