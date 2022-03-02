6ae45615c336a1755baa9d80e8fb05689b536081
palerdot <palerdot@gmail.com>
2022-03-02 09:20:17 +0530

    test: dummy test file

--- test/slite.test.tsx
+++ test/slite.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as Slite } from '../stories/Thing.stories';
 
-describe('Thing', () => {
-  it('renders without crashing', () => {
+describe('Slite', () => {
+  test('render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Slite />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
