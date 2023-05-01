d3f622a459b3565435fe6bc7d62eeba9cdaf0b54
palerdot <palerdot@gmail.com>
2023-05-01 13:04:27 +0530

    cleaning up stories

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-function Slite() {
-  return null
+import React, { FC } from 'react'
+
+const Slite: FC = () => {
+  return <div>{'porumai ... wait and hope ... lexical powered slite ... '}</div>
 }
 
 export default Slite
---
+++ src/stories/slite.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Slite from '../index'
+
+// ref: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Slite> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Slite',
+  component: Slite,
+}
+
+export default meta
+type Story = StoryObj<typeof Slite>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+  name: 'Default Slite',
+  args: {},
+}
