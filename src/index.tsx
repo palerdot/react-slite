640df90987b6fde35e9a692fa34830833366ce46
palerdot <palerdot@gmail.com>
2023-05-02 19:47:12 +0530

    export theme classes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import Editor, { SliteProps } from './Editor'
 import './styles.css'
 
+import ThemeClassList, {
+  SLITE_EDITOR_CONTAINER_CLASS,
+} from './themes/DefaultTheme'
+
+export { ThemeClassList, SLITE_EDITOR_CONTAINER_CLASS }
+
 function Slite({ initialText, onChange, readOnly }: SliteProps) {
   return (
     <div>
